bc471985e9fd589f669d8fc042c89a313cbaa6ea
Rafagreco <78920991+Rafagreco@users.noreply.github.com>
2022-03-21 16:12:50 -0300

    Update codigo.ino

--- exercicios/ex4/codigo.ino
+++ exercicios/ex4/codigo.ino
@@ -1,1 +1,20 @@
+const int led = 2;
+const int botao = 4;
+int botaoEstado = 0 ;
+
+void setup ()
+{
+  pinMode (led, OUTPUT);
+  pinMode (botao, INPUT);
+}
+
+void loop ()
+{
+  botaoEstado = digitalRead(botao);
+  if (botaoEstado == HIGH) {
+  digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
 
