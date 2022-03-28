57a864248e29f0d3cb0f07a6650ddfa3d6bd35af
Rafagreco <78920991+Rafagreco@users.noreply.github.com>
2022-03-28 09:39:21 -0300

    Update codigo.ino

--- potenciometro/codigo.ino
+++ potenciometro/codigo.ino
@@ -1,1 +1,24 @@
+const int pot = 0;
+const int led = 3;
 
+void setup ()
+{
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+}
+
+int var;
+int var2;
+
+void loop()
+{
+  var = analogRead(pot);
+  Serial.println(var);
+  var2 = map(var,0,1023,0,255);
+  analogWrite(led,var2);
+  Serial.print("pot: ");
+  Serial.println(var);
+  Serial.print("map: ");
+  Serial.println(var2);
+  delay(100);
+}
